e102118ee4bd691d1bc058737e50ea01ac75c688
Ferg <121174140+Fergdgreat@users.noreply.github.com>
2023-05-21 22:02:27 +0800

    Create Pet.java

---
+++ Exercise3/Cat/Pet.java
@@ -0,0 +1,10 @@
+public class Pet implements AmericanCurl {
+   
+    @Override
+    public void MEOWWWWWMEOWWWW() {
+        System.out.println();
+        System.out.println("My favorite AmericanCurl is meowing");
+        System.out.println();
+    };
+    
+}
