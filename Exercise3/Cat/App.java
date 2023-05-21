0bdb7be0998a6d2cf69ae211fd7ae9bb2296a561
Ferg <121174140+Fergdgreat@users.noreply.github.com>
2023-05-21 20:36:36 +0800

    Create App.java

---
+++ Exercise3/Cat/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+           Pet myPet = My new Pet();
+
+           
+           myPet.meow();
+           System.out.println("His weight is " + myPet.weight + " and his height is " + myPet.height);
+           System.out.println();
+           System.out.println("The color of his eyes is " + myPet.eyecolor + " and he is " + myPet.characteristic);
+           System.out.println();
+    }
+}                   
