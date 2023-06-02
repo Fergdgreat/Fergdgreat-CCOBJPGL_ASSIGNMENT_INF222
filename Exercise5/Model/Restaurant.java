998d1b12eacd9ee120453461bc871f00000f45f8
Ferg <121174140+Fergdgreat@users.noreply.github.com>
2023-06-02 22:58:10 +0800

    Create Restaurant.java

---
+++ Exercise5/Model/Restaurant.java
@@ -0,0 +1,23 @@
+package model;
+
+class Restaurant {
+
+    private String flavor;
+    private String taste;
+
+    public String getFlavor() {
+        return this.flavor;
+    }
+
+    public void setColor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public String getTaste() {
+        return this.taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+}
